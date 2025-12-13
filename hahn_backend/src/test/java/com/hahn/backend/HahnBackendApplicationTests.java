4a569138ae31f5e66aef58230461ac901435ee29
Salahjb <salah8jab@gmail.com>
2025-12-13 22:12:39 +0100

    chore: Add documentation and test files

---
+++ hahn_backend/src/test/java/com/hahn/backend/HahnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahn.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
