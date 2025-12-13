6760dadc4927fd11ec04aa1cde0347700d88f205
Salahjb <salah8jab@gmail.com>
2025-12-13 21:51:46 +0100

    chore: Initialize Spring Boot project structure and build tools

---
+++ hahn_backend/src/main/java/com/hahn/backend/HahnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hahn.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HahnBackendApplication.class, args);
+    }
+
+}
